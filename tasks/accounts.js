969464b1182938a5608edbe142d57696943c2104
Nick Barry <itsnickbarry@protonmail.ch>
2022-10-09 22:22:15 -0500

    use chalk and cli-table3, output table of accounts and balances

--- tasks/accounts.js
+++ tasks/accounts.js
@@ -1,3 +1,6 @@
+const chalk = require('chalk');
+const Table = require('cli-table3');
+
 task(
   'accounts', 'Output list of available accounts'
 ).setAction(async function (args, hre) {
@@ -8,8 +11,33 @@ task(
     account => provider.send('eth_getBalance', [account])
   ));
 
+  const table = new Table({
+    head: [chalk.bold('Account'), chalk.bold('Native Balance (wei)')],
+    style: { head: [], border: [], 'padding-left': 2, 'padding-right': 2 },
+    chars: {
+      mid: '·',
+      'top-mid': '|',
+      'left-mid': ' ·',
+      'mid-mid': '|',
+      'right-mid': '·',
+      left: ' |',
+      'top-left': ' ·',
+      'top-right': '·',
+      'bottom-left': ' ·',
+      'bottom-right': '·',
+      middle: '·',
+      top: '-',
+      bottom: '-',
+      'bottom-mid': '|',
+    },
+  });
+
   for (let i = 0; i < accounts.length; i++) {
-    console.log(accounts[i]);
-    console.log(balances[i]);
+    table.push([
+      { content: accounts[i] },
+      { content: balances[i] },
+    ]);
   }
+
+  console.log(table.toString());
 });
