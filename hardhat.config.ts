077845d4ce4c26fb2e409118822613209174fa31
Nick Barry <itsnickbarry@protonmail.ch>
2024-07-23 17:23:53 +0300

    configure networks and use Whynot63 fork of hardhat-change-network plugin

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,22 @@
 import './src/index';
+import 'hardhat-change-network';
 import { HardhatUserConfig } from 'hardhat/config';
 
-const config: HardhatUserConfig = {};
+const config: HardhatUserConfig = {
+  networks: {
+    ethereum: {
+      url: 'https://eth.llamarpc.com',
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk',
+      },
+    },
+    arbitrum: {
+      url: 'https://endpoints.omniatech.io/v1/arbitrum/one/public',
+      accounts: {
+        mnemonic: 'test test test test test test test test test test test junk',
+      },
+    },
+  },
+};
 
 export default config;
