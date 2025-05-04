f0843fe78ef7af733e49be552326eb33f4323227
Nick Barry <itsnickbarry@protonmail.ch>
2025-05-04 15:37:34 -0600

    set network types in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,12 +5,14 @@ const config: HardhatUserConfig = {
   plugins: [HardhatAccounts],
   networks: {
     ethereum: {
+      type: 'http',
       url: 'https://eth.llamarpc.com',
       accounts: {
         mnemonic: 'test test test test test test test test test test test junk',
       },
     },
     arbitrum: {
+      type: 'http',
       url: 'https://endpoints.omniatech.io/v1/arbitrum/one/public',
       accounts: {
         mnemonic: 'test test test test test test test test test test test junk',
